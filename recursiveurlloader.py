7ecba4b13095c0e2e686fef553538366a6b1d4c2
ginnoyong <yongginno@gmail.com>
2024-10-18 12:20:08 +0800

    v0.1

--- recursiveurlloader.py
+++ recursiveurlloader.py
@@ -1,4 +1,4 @@
-#~~~~~~~~ Loader Code
+#~~~~~~~~ RecursiveUrlLoader Code
 from langchain_community.document_loaders import RecursiveUrlLoader
 import re
 from bs4 import BeautifulSoup
@@ -10,7 +10,8 @@ def bs4_extractor(html: str) -> str:
 loader = RecursiveUrlLoader(
     "https://www.moe.gov.sg/post-secondary/admissions",
     #"https://docs.python.org/3.9/",
-    # extractor=bs4_extractor,
+    #~~~ do not use bs4_extractor if using HTML splitters
+    #extractor=bs4_extractor,
     max_depth=3,
     # use_async=False,
     # metadata_extractor=None,
@@ -24,41 +25,88 @@ loader = RecursiveUrlLoader(
 )
 
 docs = loader.load()
-#print(docs[0].metadata)
+#print(f"""\n\n#####\n{docs[0].metadata}\n{docs[0].page_content}\n""")
+#print(f"""\n\n#####\n{docs[1].metadata}\n{docs[1].page_content}\n""")
 
 #~~~~~~~~ If using HTMLSectionSplitter Code
-#~~~~~ RecursiveUrlLoader gives metadata['title'] but HTMLSectionSplitter.split_documents expects metadata['Title'] 
+#~~~~ RecursiveUrlLoader gives metadata['title'] but HTMLSectionSplitter.split_documents expects metadata['Title'] 
 for doc in docs:
-    doc.metadata['Title']=doc.metadata.get('title')
-# print(docs[0].metadata)
+   doc.metadata['Title']=doc.metadata.get('title')
+#print(docs[0].metadata)
 
-from langchain_text_splitters import HTMLSectionSplitter
-headers_to_split_on = [("h1", "Header 1"), ("h2", "Header 2"), ("h3", "Header 3")]
-html_splitter = HTMLSectionSplitter(headers_to_split_on=headers_to_split_on)
-html_splitted_docs = html_splitter.split_documents(documents=(docs))
-#print(len(html_splitted_docs))
 
 #~~~~~~~~ Embeddings code
 from langchain_openai import OpenAIEmbeddings
 embeddings_model = OpenAIEmbeddings(model='text-embedding-3-small')
 
-#~~~~~~~~ If using SemanticChunker Code
-#from langchain_experimental.text_splitter import SemanticChunker
+
+#~~~~~~~~ Select type of splitters to use, remove comment tags to use
+#~~~~~~~~ splitter: HTMLSectionSplitter
+from langchain_text_splitters import HTMLSectionSplitter
+headers_to_split_on = [("h1", "Header 1"), ("h2", "Header 2"), ]
+html_splitter = HTMLSectionSplitter(headers_to_split_on=headers_to_split_on)
+#~~~ comment out if using ParentDocumentRetriever
+html_splitted_docs = html_splitter.split_documents(documents=(docs))
+#print(len(html_splitted_docs))
+
+
+#~~~~~~~~ slitter: SemanticChunker 
+from langchain_experimental.text_splitter import SemanticChunker
 
 # Create the text splitter
-#semantic_text_splitter = SemanticChunker(embeddings_model)
+semantic_text_splitter = SemanticChunker(embeddings_model)
 
-# Split the documents into smaller chunks
-#semantic_splitted_documents = semantic_text_splitter.split_documents(docs)
-#print(len(semantic_splitted_documents))
+#~~~~~~~~ slitter: RecursiveCharacterTextSplitter
+#from langchain_text_splitters import RecursiveCharacterTextSplitter
+#text_splitter = RecursiveCharacterTextSplitter(
+#    separators=["\n\n", "\n", " ", ""],
+#    chunk_size=500,
+    # chunk_overlap=50,
+    # length_function=count_tokens
+#)
+
+
+#~~~~~~~~ load pdf doc: PyPDFLoader
+file_path = "https://www.moe.gov.sg/-/media/files/post-secondary/a-guide-to-post-secondary-admissions-exercises.pdf"
+
+from langchain_community.document_loaders import PyPDFLoader
+ 
+loader = PyPDFLoader(file_path)
+pdf_docs = loader.load()
+
+splitted_pdf_docs = semantic_text_splitter.split_documents(pdf_docs)
+#~~~~~~~~
+
+combined_splitted_docs = []
+combined_splitted_docs.extend(splitted_pdf_docs)
+combined_splitted_docs.extend(html_splitted_docs)
 
 #~~~~~~~~ Chroma Vector Store code
-# from langchain_community.vectorstores import Chroma
 from langchain_chroma import Chroma
 
-vectordb = Chroma.from_documents(html_splitted_docs, embeddings_model, collection_name='admissions', persist_directory='./vector_db')
+#~~~ comment out if using ParentDocumentRetriever
+vectordb = Chroma.from_documents(combined_splitted_docs, embeddings_model, collection_name='admissions', persist_directory='./vector_db')
 # vectordb.reset_collection()
 # print(f"########:{len(vectordb.get()['documents'])}")
+
+#~~~~~~~~ ParentDocumentRetriever code
+#from langchain.storage import InMemoryStore
+#from langchain.retrievers import ParentDocumentRetriever
+#from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+#child_splitter = RecursiveCharacterTextSplitter.from_language('html')
+#print(RecursiveCharacterTextSplitter.get_separators_for_language('html'))
+#store = InMemoryStore()
+#vectordb = Chroma(embedding_function=embeddings_model, collection_name='admissions', persist_directory='./vector_db')
+#retriever = ParentDocumentRetriever(
+#    vectorstore=vectordb,
+#    docstore=store,
+#    child_splitter=child_splitter,
+#    search_kwargs={"fetch_k": 20,"k":5}, 
+#    search_type="mmr",
+#)
+
+#retriever.add_documents(docs, ids=None)
 
 #~~~~~~~~ Prompt Template code
 from langchain.prompts import PromptTemplate
@@ -66,7 +114,10 @@ from langchain.prompts import PromptTemplate
 # Build prompt
 template = """
 Use the following pieces of context, delimited by <context> to answer the question at the end. \
+Note that Direct-Entry-Scheme to Polytechnic Programme (DPP) and Polytechnic Foundation Programme (PFP) \
+    are also Admission Exercises. 
 If you don't know the answer, just say that you don't know, do not make up answers. \
+Do not make up admission exercises that do not exist. 
 Keep the answer as concise as possible. 
 Always say "Hope this answers your question!" at the end of your answer. 
 <context>
@@ -83,7 +134,8 @@ from langchain_openai import ChatOpenAI
 #from langchain.retrievers.multi_query import MultiQueryRetriever
 
 llm = ChatOpenAI(model='gpt-4o-mini', temperature=0)
-retriever=vectordb.as_retriever(search_kwargs={"k": 20}, search_type="mmr")
+#~~~ comment out if using ParentDocumentRetriever
+retriever=vectordb.as_retriever(search_kwargs={"k":5, "fetch_k":25}, search_type="mmr")
 
 #retriever_multiquery = MultiQueryRetriever.from_llm(
 #  retriever=retriever, llm=llm,
@@ -101,7 +153,7 @@ qa_chain = RetrievalQA.from_chain_type(
 )
 
 #~~~~~~~~ Invoke and Response
-response = qa_chain.invoke("is the PFP programme admission exercise for N or o-level students?")
+response = qa_chain.invoke("I have a N-level cert and i want to join a poly. what admission exercise should i register for?")
 
 #print(response)
 #print(len(response.get('source_documents')))
